17c4b457f81b29abe1bfb03ba598d4a3f628604d
sumonpaul55 <sumpaul100@gmail.com>
2024-10-13 21:53:14 +0600

    payment create successfully there was a problem now its fixed

--- dist/app/moduls/category/category.route.js
+++ dist/app/moduls/category/category.route.js
@@ -11,6 +11,6 @@ const validateRequest_1 = __importDefault(require("../../middleWare/validateRequ
 const category_validation_1 = require("./category.validation");
 const category_controller_1 = require("./category.controller");
 const router = (0, express_1.Router)();
-router.post("/", (0, authGaurd_1.default)(user_constant_1.USER_ROLE.USER), (0, validateRequest_1.default)(category_validation_1.categoryValidation.categoryValidationSchema), category_controller_1.categoryController.createCatergory);
-router.get("/", (0, authGaurd_1.default)(user_constant_1.USER_ROLE.USER, user_constant_1.USER_ROLE.ADMIN), category_controller_1.categoryController.getAllCategory);
+router.post("/", (0, authGaurd_1.default)(user_constant_1.USER_ROLE.ADMIN), (0, validateRequest_1.default)(category_validation_1.categoryValidation.categoryValidationSchema), category_controller_1.categoryController.createCatergory);
+router.get("/", category_controller_1.categoryController.getAllCategory);
 exports.categoryRouter = router;
