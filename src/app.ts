dd37f89b74a498cd2aae8c558b157d091ea4cf12
sumonpaul55 <sumpaul100@gmail.com>
2025-04-01 15:25:52 +0600

    credential

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 
 app.use(cookieParser());
 // app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
-app.use(cors({ origin: ["http://localhost:3000", "https://gardenigng-tips-advice.vercel.app","http://64.23.231.38:2626"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "https://gardenigng-tips-advice.vercel.app", "http://64.23.231.38:2626"], credentials: true }));
 
 // parser
 app.use(express.json());
